801a8311cc024ff942c890430866fca1c66ccac3
cja-smith <62566300+cja-smith@users.noreply.github.com>
2020-07-24 13:03:51 +0100

    Add files via upload
    Simple password generator that user inputs how long they want the password to be.

---
+++ simple_pword_generator.py
@@ -0,0 +1,32 @@
+def pword_gen():
+    
+    from secrets import randbelow
+    import string
+    #setting up a list of possible symbols for the password
+    pword_symbols_list = string.ascii_letters+string.digits+string.punctuation
+    pword = ''
+    
+    while True:
+        pword_length = input("How many characters do you want this password to be?")
+        
+        try: 
+            pword_length = int(pword_length)
+            
+            if pword_length <= 0: #if the input is not a positive integer, retry
+                print("Sorry, input must be positive and non-zero.")
+                continue
+            else:
+                break
+                
+        except ValueError: #if the input is not a number, retry
+            print("Input a positive integer, please.")
+            continue
+    
+    for i in range(pword_length):
+        pword += pword_symbols_list[randbelow(94)]
+        
+    print(f"Your new password is:\n{pword}")
+    print("Keep it safe!")
+
+
+pword_gen()
